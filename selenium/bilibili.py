61295270f706cebbdf33ffdbe716ae14abf0972f
windsnow1025 <windsnow1025@gmail.com>
2024-03-26 04:07:02 +0000

    selenium search

--- selenium/bilibili.py
+++ selenium/bilibili.py
@@ -7,8 +7,11 @@ class Bilibili:
     def __init__(self, webdriver):
         self.driver = webdriver
         self.wait = WebDriverWait(self.driver, timeout=4)
-        self.search_input_path = "//input[@placeholder='搜索']"
+        self.search_input_path = "//input[@class='nav-search-input']"
+        self.search_button_path = "//div[@class='nav-search-btn']"
 
     def search(self):
         search_input = self.wait.until(presence_of_element_located((By.XPATH, self.search_input_path)))
         search_input.send_keys("前端")
+        search_button = self.wait.until(presence_of_element_located((By.XPATH, self.search_button_path)))
+        search_button.click()
