1769f625c4aecdae72d51c20a20572054cfa5024
windsnow1025 <windsnow1025@gmail.com>
2024-03-26 15:25:36 +0000

    selenium scroll and save csv

--- selenium/bilibili.py
+++ selenium/bilibili.py
@@ -1,8 +1,9 @@
+from selenium.common import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.expected_conditions import *
 from selenium.webdriver.support.wait import WebDriverWait
 
-from user import User
+from user import *
 
 
 class Bilibili:
@@ -23,28 +24,49 @@ class Bilibili:
         search_button = self.wait.until(presence_of_element_located((By.XPATH, self.search_button_path)))
         search_button.click()
 
-    def process_cards(self):
-        card_divs = self.wait.until(presence_of_all_elements_located((By.XPATH, self.card_div_path)))
+    def process_cards(self, target_card_count=12):
         users = []
-        for card_div in card_divs:
+        last_count = 0
+
+        while len(users) < target_card_count:
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
             try:
-                user = self.process_card(card_div)
-                users.append(user)
-            except Exception as e:
-                print(e)
+                self.wait.until(
+                    lambda d: len(d.find_elements(By.XPATH, self.card_div_path)) > last_count
+                )
+            except TimeoutException:
+                print("Timed out waiting for more cards to load.")
                 break
 
-        for user in users:
-            print(user.name, user.up, user.pubdate, user.course_desc)
+            card_divs = self.driver.find_elements(By.XPATH, self.card_div_path)
+            last_count = len(card_divs)
+
+            for card_div in card_divs[len(users):target_card_count]:
+                try:
+                    user = self.process_card(card_div)
+                    users.append(user)
+                except Exception as e:
+                    print(e)
+                    break
+
+                if len(users) >= target_card_count:
+                    break
+
+        save_users_to_csv(users, "users.csv")
 
     def process_card(self, card_div: WebElement) -> User:
         user = User()
         user.name = card_div.find_element(By.XPATH, self.title_h3_path).text
         user.up = card_div.find_element(By.XPATH, self.author_span_path).text
         user.pubdate = card_div.find_element(By.XPATH, self.date_span_path).text
-        card_div.click()
-        self.driver.switch_to.window(self.driver.window_handles[2])
-        user.course_desc = self.wait.until(presence_of_element_located((By.XPATH, self.description_span_path))).text
-        self.driver.close()
-        self.driver.switch_to.window(self.driver.window_handles[1])
-        return user
+        try:
+            card_div.click()
+            self.driver.switch_to.window(self.driver.window_handles[2])
+            user.course_desc = self.wait.until(presence_of_element_located((By.XPATH, self.description_span_path))).text
+        except Exception as e:
+            raise e
+        finally:
+            self.driver.close()
+            self.driver.switch_to.window(self.driver.window_handles[1])
+            return user
--- selenium/user.py
+++ selenium/user.py
@@ -1,3 +1,23 @@
+import csv
+
+
+def save_users_to_csv(users, file_path):
+    headers = ['name', 'up', 'course_desc', 'pubdate', 'url', 'card_url', 'course_like', 'course_fav', 'course_view', 'tag']
+
+    with open(file_path, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+
+        writer.writerow(headers)
+
+        for user in users:
+            row = [
+                user.name, user.up, user.course_desc, user.pubdate,
+                user.url, user.card_url, user.course_like, user.course_fav,
+                user.course_view, user.tag
+            ]
+            writer.writerow(row)
+
+
 class User:
     def __init__(self):
         self.name = ""
