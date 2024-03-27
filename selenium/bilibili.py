4360c8644af0540066ab371ef851ebf6e480937c
windsnow1025 <windsnow1025@gmail.com>
2024-03-27 07:54:36 +0000

    selenium max played

--- selenium/bilibili.py
+++ selenium/bilibili.py
@@ -12,12 +12,13 @@ class Bilibili:
         self.wait = WebDriverWait(self.driver, timeout=4)
         self.search_input_path = "//input[@class='nav-search-input']"
         self.search_button_path = "//div[@class='nav-search-btn']"
+        self.max_played_button_path = "//button[text()='最多播放']"
         self.card_div_path = "//div[@class='bili-video-card']"
+        self.close_div_path = "//div[@class='close']"
         self.title_h3_path = ".//h3[@class='bili-video-card__info--tit']"
         self.author_span_path = ".//span[@class='bili-video-card__info--author']"
-        self.date_div_path = ".//div[@class='pubdate']"
+        self.date_div_path = "//div[@class='pubdate']"
         self.description_span_path = "//span[@class='desc-info-text']"
-        self.close_div_path = "//div[@class='close']"
 
     def search(self):
         search_input = self.wait.until(presence_of_element_located((By.XPATH, self.search_input_path)))
@@ -27,13 +28,17 @@ class Bilibili:
         self.driver.close()
         self.driver.switch_to.window(self.driver.window_handles[0])
 
-    def process_cards(self, target_card_count=25):
+    def process_cards(self, target_card_count=24):
         users = []
         last_count = 0
 
+        self.wait.until(presence_of_element_located((By.XPATH, self.max_played_button_path))).click()
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        self.wait.until(presence_of_element_located((By.XPATH, self.close_div_path))).click()
+        self.driver.execute_script("window.scrollTo(0, 0);")
+
         while len(users) < target_card_count:
             self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            self.wait.until(presence_of_element_located((By.XPATH, self.close_div_path))).click()
 
             try:
                 self.wait.until(
@@ -43,7 +48,7 @@ class Bilibili:
                 print("Timed out waiting for more cards to load.")
                 break
 
-            card_divs = self.driver.find_elements(By.XPATH, self.card_div_path)
+            card_divs = self.wait.until(presence_of_all_elements_located((By.XPATH, self.card_div_path)))
             last_count = len(card_divs)
 
             try:
