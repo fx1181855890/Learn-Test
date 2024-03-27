15e0a9eee6fdda175976e29d49e3b87d64b9c147
windsnow1025 <windsnow1025@gmail.com>
2024-03-27 08:05:11 +0000

    selenium custom wait find class member

--- selenium/bilibili.py
+++ selenium/bilibili.py
@@ -10,6 +10,8 @@ class Bilibili:
     def __init__(self, webdriver: WebDriver):
         self.driver = webdriver
         self.wait = WebDriverWait(self.driver, timeout=4)
+        self.wait_find = lambda path: self.wait.until(presence_of_element_located((By.XPATH, path)))
+        self.wait_find_all = lambda path: self.wait.until(presence_of_all_elements_located((By.XPATH, path)))
         self.search_input_path = "//input[@class='nav-search-input']"
         self.search_button_path = "//div[@class='nav-search-btn']"
         self.max_played_button_path = "//button[text()='最多播放']"
@@ -21,9 +23,9 @@ class Bilibili:
         self.description_span_path = "//span[@class='desc-info-text']"
 
     def search(self):
-        search_input = self.wait.until(presence_of_element_located((By.XPATH, self.search_input_path)))
+        search_input = self.wait_find(self.search_input_path)
         search_input.send_keys("前端")
-        search_button = self.wait.until(presence_of_element_located((By.XPATH, self.search_button_path)))
+        search_button = self.wait_find(self.search_button_path)
         search_button.click()
         self.driver.close()
         self.driver.switch_to.window(self.driver.window_handles[0])
@@ -32,9 +34,9 @@ class Bilibili:
         users = []
         last_count = 0
 
-        self.wait.until(presence_of_element_located((By.XPATH, self.max_played_button_path))).click()
+        self.wait_find(self.max_played_button_path).click()
         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        self.wait.until(presence_of_element_located((By.XPATH, self.close_div_path))).click()
+        self.wait_find(self.close_div_path).click()
         self.driver.execute_script("window.scrollTo(0, 0);")
 
         while len(users) < target_card_count:
@@ -48,7 +50,7 @@ class Bilibili:
                 print("Timed out waiting for more cards to load.")
                 break
 
-            card_divs = self.wait.until(presence_of_all_elements_located((By.XPATH, self.card_div_path)))
+            card_divs = self.wait_find_all(self.card_div_path)
             last_count = len(card_divs)
 
             try:
@@ -69,9 +71,9 @@ class Bilibili:
         try:
             card_div.click()
             self.driver.switch_to.window(self.driver.window_handles[1])
-            user.pubdate = self.wait.until(presence_of_element_located((By.XPATH, self.date_div_path))).text
+            user.pubdate = self.wait_find(self.date_div_path).text
             try:
-                user.course_desc = self.wait.until(presence_of_element_located((By.XPATH, self.description_span_path))).text
+                user.course_desc = self.wait_find(self.description_span_path).text
             except Exception:
                 user.course_desc = ""
         except Exception as e:
