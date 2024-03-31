a506e11770005f4d044e0d7091564bad689125e3
windsnow1025 <windsnow1025@gmail.com>
2024-03-31 19:17:15 +0000

    selenium all path

--- selenium/bilibili.py
+++ selenium/bilibili.py
@@ -19,15 +19,23 @@ class Bilibili:
     def __init__(self, webdriver: WebDriver):
         self.driver = webdriver
         self.url = "https://search.bilibili.com/all?keyword=%E5%89%8D%E7%AB%AF&order=click"
+
         self.search_input_path = "//input[@class='nav-search-input']"
         self.search_button_path = "//div[@class='nav-search-btn']"
         self.max_played_button_path = "//button[text()='最多播放']"
+
         self.card_div_path = "//div[@class='bili-video-card']"
         self.close_div_path = "//div[@class='close']"
         self.title_h3_path = ".//h3[@class='bili-video-card__info--tit']"
         self.author_span_path = ".//span[@class='bili-video-card__info--author']"
+        self.cover_picture_path = ".//picture[@class='v-img bili-video-card__cover']"
+
         self.date_div_path = "//div[@class='pubdate-ip-text']"
         self.description_span_path = "//span[@class='desc-info-text']"
+        self.like_span_path = "//span[@class='video-like-info video-toolbar-item-text']"
+        self.fav_span_path = "//span[@class='video-fav-info video-toolbar-item-text']"
+        self.view_div_path = "//div[@class='view-text']"
+        self.tag_div_path = "//div[@class='tag not-btn-tag']"
 
     def __wait(self, element: WebDriver | WebElement = None) -> WebDriverWait:
         if element is None:
@@ -50,7 +58,7 @@ class Bilibili:
         self.driver.close()
         self.driver.switch_to.window(self.driver.window_handles[0])
 
-    def process_cards(self, target_card_count=24):
+    def process_cards(self, target_card_count=4):
         users = []
         last_count = 0
 
@@ -60,7 +68,7 @@ class Bilibili:
         self.driver.execute_script("window.scrollTo(0, 0);")
 
         while len(users) < target_card_count:
-            # self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
             try:
                 self.__wait().until(
@@ -89,11 +97,18 @@ class Bilibili:
         user = User()
         user.name = self.__wait_find(self.title_h3_path, element=card_div).text
         user.up = card_div.find_element(By.XPATH, self.author_span_path).text
+        user.card_url = card_div.find_element(By.XPATH, f"{self.cover_picture_path}/img").get_attribute("src")
+
         try:
             card_div.click()
             self.driver.switch_to.window(self.driver.window_handles[1])
             user.url = get_base_url(self.driver.current_url)
             user.pubdate = self.__wait_find(self.date_div_path).text
+            user.course_like = self.__wait_find(self.like_span_path).text
+            user.course_fav = self.__wait_find(self.fav_span_path).text
+            user.course_view = self.__wait_find(self.view_div_path).text
+            tags = self.__wait_find(self.tag_div_path, all=True)
+            user.tag = [tag.text for tag in tags]
             try:
                 user.course_desc = self.__wait_find(self.description_span_path).text
             except Exception:
