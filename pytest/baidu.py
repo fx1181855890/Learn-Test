eb88e7d170094068554d033b31934b2c792a8f14
windsnow1025 <windsnow1025@gmail.com>
2024-04-03 06:53:40 +0000

    pytest reset to homepage

--- pytest/baidu.py
+++ pytest/baidu.py
@@ -6,13 +6,17 @@ from scraper import Scraper
 
 class Baidu(Scraper):
     def __init__(self):
-        super().__init__("https://www.baidu.com")
+        self.base_url = "https://www.baidu.com"
+        super().__init__(self.base_url)
 
         self.search_input_path = "//input[@name='wd']"
 
-        self.setting_a_path = "//a[contains(text(),'设置')]"
+        self.setting_span_path = "//span[contains(text(),'设置')]"
         self.search_setting_a_path = "//span[contains(text(),'搜索设置')]"
         self.save_setting_a_path = "//a[contains(text(),'保存设置')]"
+
+    def reset_to_homepage(self):
+        self.driver.get(self.base_url)
 
     def search(self):
         search_box = self._wait_find(self.search_input_path)
@@ -23,7 +27,7 @@ class Baidu(Scraper):
         assert True
 
     def settings(self):
-        settings_link = self._wait_find(self.setting_a_path)
+        settings_link = self._wait_find(self.setting_span_path)
         settings_link.click()
 
         search_setting = self._wait_find(self.search_setting_a_path)
--- pytest/main.py
+++ pytest/main.py
@@ -3,6 +3,7 @@ from baidu import Baidu
 try:
     baidu = Baidu()
     baidu.search()
+    baidu.reset_to_homepage()
     baidu.settings()
 except Exception as e:
     raise e
