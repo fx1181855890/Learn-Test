56c2d72c2a4fb7c8d48b0f95a3f9566e0889fa2e
windsnow1025 <windsnow1025@gmail.com>
2024-05-18 00:29:57 -0700

    discuz x unittest

--- discuz/unittest/discuz_test.py
+++ discuz/unittest/discuz_test.py
@@ -1,29 +1,42 @@
+import unittest
 from selenium import webdriver
 
 from discuz.page.board_page import BoardPage
 from discuz.page.user_page import UserPage
 from discuz.util import load_boards_from_json
 
-# Initialize WebDriver
-webdriver = webdriver.Chrome()
 
-# Sign in
-user_page = UserPage(webdriver)
-user_page.sign_in(username="admin", password="admin")
-assert user_page.is_signed_in()
+class DiscuzTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # Initialize WebDriver
+        cls.driver = webdriver.Chrome()
+        cls.user_page = UserPage(cls.driver)
+        cls.board_page = BoardPage(cls.driver)
 
-# Initialize board page
-board_page = BoardPage(webdriver)
-board_page.init_board()
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
 
-# Load boards from JSON
-loaded_boards = load_boards_from_json()
+    def setUp(self):
+        # Sign in before each test
+        self.user_page.sign_in(username="admin", password="admin")
+        self.assertTrue(self.user_page.is_signed_in(), "User should be signed in")
 
-# Add boards using the board page
-board_page.add_boards(loaded_boards)
-assert board_page.is_boards_added(loaded_boards)
+    def test_add_boards(self):
+        # Initialize board page
+        self.board_page.init_board()
 
-# Clear added boards
-board_page.clean_up()
+        # Load boards from JSON
+        loaded_boards = load_boards_from_json()
 
-print("Boards have been added.")
+        # Add boards using the board page
+        self.board_page.add_boards(loaded_boards)
+        self.assertTrue(self.board_page.is_boards_added(loaded_boards), "Boards should be added")
+
+        # Clear added boards
+        self.board_page.clean_up()
+
+
+if __name__ == "__main__":
+    unittest.main()
